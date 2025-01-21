6b393f39cffb2a2eec0708a412b4c6ada5b30c98
Jose Edgar Barroso Neto <edgarbarroso@MacBook-Air-de-Jose.local>
2025-01-21 02:31:58 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { Belt } from "./components/belt";
 import { Button } from "./components/button";
 import { FaCheckCircle } from "react-icons/fa";
 import { motion } from "motion/react";
+import { AiOutlineCaretDown } from "react-icons/ai";
 
 export default function Home() {
   return (
@@ -47,9 +48,13 @@ export default function Home() {
           <Belt />
         </div>
       </section>
-      <motion.section className="relative flex flex-col items-center gap-10 py-20 bg-background z-20">
+      <motion.section className="relative flex flex-col items-center gap-10 pt-20 bg-background z-20">
         <p className="text-gray-600 border-primary border-l-4 pl-2">Assista</p>
-        <video className="w-[800px]" controls preload="auto">
+        <video
+          className="w-[800px] rounded-2xl border-4 border-primary"
+          controls
+          preload="auto"
+        >
           <source src="/video.mp4" type="video/mp4" />
         </video>
         <Button />
@@ -161,9 +166,141 @@ export default function Home() {
           className="md:absolute right-[10%] top-1/2 rounded-3xl w-auto md:h-[400px] "
         />
       </motion.section>
-      <section className="h-screen flex flex-col items-center">
+      <div className="h-20 mb-10 flex flex-col items-center">
         <Button />
-      </section>
+      </div>
+
+      <motion.section
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+        viewport={{ once: true, amount: 0.1 }}
+        className="flex flex-col bg-[#ECE7E3] w-full items-center py-10 gap-10 text-secondary px-[10vw] max-md:px-[5vw] md:max-h-[50vh] md:mb-[50vh]"
+      >
+        <p className="text-gray-600 border-primary border-l-4 pl-2">
+          Depoimentos
+        </p>
+        <h3 className=" text-3xl max-md:text-center ">
+          Comentários de quem ja transformou sua vida
+        </h3>
+        <div className="flex flex-row items-center justify-center">
+          <Image
+            src={"/depoimento-1.png"}
+            alt={""}
+            width={500}
+            height={500}
+            className=" "
+          />
+          <Image
+            src={"/depoimento-2.png"}
+            alt={""}
+            width={500}
+            height={500}
+            className="max-md:hidden "
+          />
+          <Image
+            src={"/depoimento-3.png"}
+            alt={""}
+            width={500}
+            height={500}
+            className="max-md:hidden "
+          />
+        </div>
+      </motion.section>
+      <motion.section
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+        viewport={{ once: true, amount: 0.1 }}
+        className="flex flex-col  w-full items-center py-10 gap-10 text-secondary px-[10vw] max-md:px-[5vw]"
+      >
+        <p className="text-gray-600 border-primary border-l-4 pl-2">Dúvidas</p>
+        <h3 className=" text-3xl max-md:text-center ">Perguntas Frequentes</h3>
+        <div className="flex flex-col gap-1 w-full ">
+          {frequentlyAskedQuestions.map(({ question, answer }) => (
+            <div key={question}>
+              <div className="flex flex-row justify-between rounded-t-2xl  bg-secondary text-gray-100 px-6 py-4 items-center">
+                <p className="text-xl max-md:text-sm gap-4 ">{question}</p>
+                <AiOutlineCaretDown className="text-xl" size={30} />
+              </div>
+              <div className="flex flex-row justify-between rounded-b-2xl  bg-primary text-gray-100 px-6 py-4 items-center">
+                <p className="text-xl max-md:text-sm gap-4 ">{answer}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </motion.section>
+      <motion.section
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+        viewport={{ once: true }}
+        className="relative flex flex-row max-md:flex-col max-md:gap-0 items-center justify-center  px-[10vw] max-md:px-[5vw] bg-[#ECE7E3]"
+      >
+        <div className="flex flex-col max-md:items-center">
+          <Image
+            width={200}
+            height={200}
+            alt="imagem logotipo"
+            src={"/logotipo.png"}
+          />
+          <h1 className="text-4xl font-extrabold max-md:text-4xl text-gray-700">
+            Não perca essa chance! Transforme sua vida em{" "}
+            <span className="text-secondary">apenas 30 dias!</span>
+          </h1>
+          <h2 className="text-lg font-light max-md:text-sm">
+            Um método prático, eficaz e comprovado para dizer adeus ás
+            gordurinhas indesejadas
+          </h2>
+          <div className="max-md:hidden ">
+            <Button />
+          </div>
+        </div>
+        <Image
+          width={400}
+          height={700}
+          alt="imagem logotipo"
+          className="max-md:-mt-20"
+          src={"/lorenna-sem-fundo.png"}
+        />
+        <div className="absolute bottom-[20%] left-1/2 -translate-x-1/2 md:hidden">
+          <Button />
+        </div>
+      </motion.section>
+      <motion.section className="h-40"></motion.section>
+      <footer className="flex flex-col items-center text-secondary bg-[#ECE7E3] py-2 text-sm">
+        <Image
+          width={120}
+          height={80}
+          alt="imagem logotipo"
+          src={"/logotipo.png"}
+        />
+        <p className="font-bold">@Copyright | Desafio 30 dias</p>
+        <p>todos os direitos reservados</p>
+      </footer>
     </main>
   );
 }
+
+const frequentlyAskedQuestions = [
+  {
+    question: "O que acontece se eu tiver dúvidas durante o desafio?",
+    answer:
+      "Você terá suporte diário pelo WhatsApp. Estou aqui para responder suas dúvidas, ajustar o plano e te dar o incentivo necessário para seguir firme no seu objetivo.",
+  },
+  {
+    question: "Eu preciso comprar alimentos caros ou difíceis de encontrar?",
+    answer:
+      "Não! O plano é pensado para ser acessível e flexível, com alimentos simples e fáceis de encontrar. Você poderá adaptar as sugestões ao que já consome no dia a dia, dentro do seu orçamento.",
+  },
+  {
+    question: "Preciso fazer exercícios físicos para participar?",
+    answer:
+      "Os exercícios físicos são recomendados, mas não obrigatórios. O foco principal está em ajustes na alimentação e em hábitos saudáveis. Se puder se movimentar, mesmo que de forma leve, isso será um ótimo complemento para seus resultados!",
+  },
+  {
+    question: "E se eu não conseguir seguir o plano perfeitamente?",
+    answer:
+      "Não tem problema! O desafio é sobre fazer o melhor dentro da sua realidade. Pequenos avanços diários já fazem diferença. O acompanhamento pelo WhatsApp será sua ferramenta para ajustar o plano, tirar dúvidas e manter o foco, mesmo que ocorram deslizes.",
+  },
+];
